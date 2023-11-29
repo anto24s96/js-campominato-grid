845cb6bdca872ab9a6d54ff862bf50e1c44fdf89
anto24s96 <anto24s96@gmail.com>
2023-11-29 17:19:07 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-//CREO UNA FUNZIONA PER DETERMINARE LA CELLA
+//CREO UNA FUNZIONOA CHE MI GENERA IL CAMPO DA GIOCO
+function createNewgame(){
+    document.getElementById("grid").innerHTML = '';  
+}
+
+
+
+document.getElementById("play").addEventListener("click", function(){
+    createNewgame();
+
+    //CREO UNA FUNZIONA PER DETERMINARE LA CELLA
 function createdCell(numbers){
     const element = document.createElement("div");
     element.classList.add("square");
@@ -17,6 +27,14 @@ for(let i = 0; i < 100; i++){
     //CREO LA CELLA
     let square = createdCell(i+1);
 
+    //AGGIUNGO AI QUADRATI UNA FUNZIONE DI "CLICK"
+    square.addEventListener("click", function(){
+        this.classList.toggle("clicked");
+        
+        console.log(i+1);
+    })
+
     //APPENDO LA CELLA ALLA GRIGLIA
     grid.appendChild(square);
 }
+})
